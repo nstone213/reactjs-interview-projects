0a5140021cc913647a8fb500542858cac17f81f6
nstone213 <nbaronstone@gmail.com>
2024-05-30 19:04:13 -0400

    Began work on project 1, accordian

---
+++ src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: "What are accordian components?",
+        answer: "Accordian components are user interface elements",
+    },
+    {
+        id: '2',
+        question: "What are they used for?",
+        answer: "They are commonly employed in various contexts",
+    },
+    {
+        id: '3',
+        question: "Accordian as a musical instrument",
+        answer: "The accordian is a musical instrument",
+    },
+    {
+        id: '4',
+        question: "Can I create an accordian component?",
+        answer: "Yes, of course",
+    },
+];
+
+export default data;
