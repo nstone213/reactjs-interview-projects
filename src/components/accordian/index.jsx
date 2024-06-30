eb46ef1d09aa7ff17a98513678e9a64ddbed974e
nstone213 <nbaronstone@gmail.com>
2024-06-30 00:18:47 -0400

    MultiSelect (@27:36)

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -8,19 +8,28 @@ import './styles.css'
 export default function Accordian() {
 
     const [selected, setSelected] = useState(null);
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
 
     function handleSingleSelection(getCurrentID) {
         console.log(getCurrentID);
         setSelected(getCurrentID === selected ? null : getCurrentID);
     }
 
+    function handleMultiSelection(getCurrentID) {
+        let cpyMultiple = [...multiple];
+        const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentID);
+
+        console.log(findIndexOfCurrentId);
+    }
+
     return <div className="wrapper">
-        <button>Enable Multi Selection</button>
+        <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
         <div className="accordian">
             {data && data.length > 0 ? (
                 data.map((dataItem) => (
                     <div className="item">
-                        <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
+                        <div onClick={enableMultiSelection ? ()=> handleMultiSelection(dataItem.id) : () => handleSingleSelection(dataItem.id)} className="title">
                             <h3>{dataItem.question}</h3>
                             <span>+</span>
                         </div>
